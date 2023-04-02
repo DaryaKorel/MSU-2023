5b4fd47618d0b680ec06556b4c2a1cf41648498a
DaryaKorel <darya-korel@mail.ru>
2023-04-02 21:57:11 +0300

    'изменения'

--- Рисуем_синусоиду.py
+++ Рисуем_синусоиду.py
@@ -5,13 +5,19 @@ from math import *
 def my_action():
     frm = int(e1.get())
     to   = int(e2.get())
+    for i in range(frm, to):
+        canvas.create_line(i, 10*sin(i)+150, i + 1, 10*sin(i)+150, fill='white', width=20)
+
+def my_action1():
+    frm = int(e1.get())
+    to   = int(e2.get())
     col = str(e3.get())
     for i in range(frm, to):
-        canvas.create_line(i, 20*sin(i)+250, i + 1, 20*sin(i)+250, fill=col, width=20)
+        canvas.create_line(i, 10*sin(i)+150, i + 1, 10*sin(i)+150, fill=col, width=20)
 
 master = Tk()
 master.title('Рисуем синусоиду')
-master.geometry('800x400')
+master.geometry('1000x400')
 
 l1 = Label(master, text='t0')
 l1.grid(row=2, column=0, sticky='e')
@@ -29,6 +35,9 @@ e3.grid(row=1, column=2)
 
 b1 = Button(master, text="Draw!", command = my_action)
 b1.grid(row=4, column = 1)
+
+b2 = Button(master, text="Изменение цвета синусоиды", command = my_action1)
+b2.grid(row=1, column = 3)
 
 canvas = Canvas(master, width=500, height=300, background='black')
 canvas.grid(column=0, row=1)
